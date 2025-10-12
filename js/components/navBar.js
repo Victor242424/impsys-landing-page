82bca568f89e83b2e8a09fa8af09d0f63ac36a10
Victor Rivas <vrivas@parameta.co>
2025-10-11 22:29:17 -0300

    Corrección de reponsividad de la navBar

--- js/components/navBar.js
+++ js/components/navBar.js
@@ -1,7 +1,15 @@
 (() => {
     document.getElementById('menuMobileButton').addEventListener('click', function() {
         const menu = document.getElementById('navBarMenu'); 
-        menu.classList.toggle('hidden');
+        const children = menu.children;
+        menu.classList.toggle('max-h-0');
+        menu.classList.toggle('opacity-0');
+        const langDropdown = document.getElementById('langDropdown');
+        langDropdown.classList.toggle('my-4');
+        for (let i = 0; i < children.length; i++) {
+            children[i].classList.toggle('translate-y-[-1rem]');
+        }
+     /*    menu.classList.toggle('hidden');  */
     });
 })();
 
